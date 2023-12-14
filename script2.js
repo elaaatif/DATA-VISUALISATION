4383abd8f93b17a97905ea30328764f0d33974f3
elaaatif <mekri.abdelatif@gmail.com>
2023-12-14 18:52:07 +0100

    14-12

--- script2.js
+++ script2.js
@@ -33,27 +33,103 @@ function updateChart() {
     const selectedYear = document.getElementById("year").value;
     const selectedQuarter = document.getElementById("quarter").value;
     const selectedLanguage = document.getElementById("language").value;
+    const showAllYears = document.getElementById("showAllYears").checked;
+    const showAllQuarters = document.getElementById("byQuarter").checked;
     const selectedVisualizationType = document.getElementById("visualizationType").value;
 
-    const filteredData = getFilteredData(selectedYear, selectedQuarter, selectedLanguage);
+    if (showAllYears || (showAllQuarters && !selectedQuarter)) {
+        // If "Show All Years" is checked or "byQuarter" is checked without a selected quarter, ignore the selected year and quarter
+        createComparisonChart(selectedLanguage, selectedVisualizationType, showAllQuarters);
+    } else {
+        const filteredData = getFilteredData(selectedYear, selectedQuarter, selectedLanguage);
+
+        // Remove previous chart
+        d3.select("#chart-container").selectAll("*").remove();
+
+        // Create new chart based on the selected visualization type
+        if (selectedVisualizationType === "line") {
+            createLineChart(filteredData, showAllQuarters);
+        } else if (selectedVisualizationType === "bar") {
+            createBarChart(filteredData, showAllQuarters);
+        }
+        // Add more conditions for other visualization types if needed
+    }
+}
+
+
+// Function to create a comparison chart for a specific language with all years included
+function createComparisonChart(language, visualizationType, showAllQuarters) {
+    let filteredData = combinedData.filter(d => d.name === language);
+
+    if (!showAllQuarters) {
+        // Filter data for the selected year
+        filteredData = filteredData.filter(d => d.year === document.getElementById("year").value);
+    }
 
     // Remove previous chart
     d3.select("#chart-container").selectAll("*").remove();
 
     // Create new chart based on the selected visualization type
-    if (selectedVisualizationType === "line") {
-        createLineChart(filteredData);
-    } else if (selectedVisualizationType === "bar") {
-        createBarChart(filteredData);
-    } else if (selectedVisualizationType === "pie") {
-        createPieChart(filteredData);
+    if (visualizationType === "line") {
+        const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+        const width = 800 - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        const svg = d3.select("#chart-container")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const x = d3.scaleBand().range([0, width]).padding(0.1);
+        const y = d3.scaleLinear().range([height, 0]);
+
+        // Set domains for x and y scales based on data
+        x.domain(filteredData.map(d => `${d.year} Q${d.quarter}`));
+        y.domain([0, d3.max(filteredData, d => d.totalCount)]);
+
+        // Add the x-axis with rotated labels
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "0.5em")
+            .attr("dx", "-0.8em")
+            .style("text-anchor", "end");
+
+        // Add the y-axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Create line function
+        const line = d3.line()
+            .x(d => x(`${d.year} Q${d.quarter}`) + x.bandwidth() / 2) // Adjusted x position
+            .y(d => y(d.totalCount));
+
+        // Append line to the chart
+        svg.append("path")
+            .datum(filteredData)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 2)
+            .attr("d", line);
+    } else if (visualizationType === "bar") {
+        createBarChart(filteredData, showAllQuarters);
     }
     // Add more conditions for other visualization types if needed
 }
 
 // Function to create a line chart
-// Function to create a line chart
-function createLineChart(data) {
+function createLineChart(data, showAllQuarters) {
+    if (!showAllQuarters) {
+        // Filter data for the selected year
+        data = data.filter(d => d.year === document.getElementById("year").value);
+    }
+
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
     const width = 800 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
@@ -83,12 +159,12 @@ function createLineChart(data) {
 
     // Create line function
     const line = d3.line()
-        .x(d => x(`${d.year} Q${d.quarter}`) + x.bandwidth() / 2) // Center line within the band
+        .x(d => x(`${d.year} Q${d.quarter}`) + x.bandwidth() / 2) // Adjusted x position
         .y(d => y(d.totalCount));
 
     // Append line to the chart
     svg.append("path")
-        .datum(data) // Use datum instead of data
+        .datum(data)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-linejoin", "round")
@@ -98,7 +174,12 @@ function createLineChart(data) {
 }
 
 // Function to create a bar chart
-function createBarChart(data) {
+function createBarChart(data, showAllQuarters) {
+    if (!showAllQuarters) {
+        // Filter data for the selected year
+        data = data.filter(d => d.year === document.getElementById("year").value);
+    }
+
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
     const width = 800 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
@@ -117,10 +198,15 @@ function createBarChart(data) {
     x.domain(data.map(d => `${d.year} Q${d.quarter}`));
     y.domain([0, d3.max(data, d => d.totalCount)]);
 
-    // Add the x-axis
+    // Add the x-axis with rotated labels
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "rotate(-90)")
+        .attr("dy", "0.5em")
+        .attr("dx", "-0.8em")
+        .style("text-anchor", "end");
 
     // Add the y-axis
     svg.append("g")
@@ -138,38 +224,49 @@ function createBarChart(data) {
         .attr("fill", "steelblue");
 }
 
+
+//-------------------------------------------------------------------------------------------------------------------------------
 // Function to create a pie chart
 function createPieChart(data) {
-    const width = 800;
+    const width = 400;
     const height = 400;
     const radius = Math.min(width, height) / 2;
 
-    const svg = d3.select("#chart-container")
+    const svg = d3.select("#pie-chart-container")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", `translate(${width / 2},${height / 2})`);
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-
+    // Create a pie chart layout
     const pie = d3.pie().value(d => d.totalCount);
 
+    // Create an arc generator
     const arc = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(0);
+        .innerRadius(0)
+        .outerRadius(radius);
 
+    // Generate pie chart arcs
     const arcs = pie(data);
 
+    // Append arcs to the chart
     svg.selectAll("path")
         .data(arcs)
         .enter().append("path")
         .attr("d", arc)
-        .attr("fill", (d, i) => color(i))
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 2)
-        .style("opacity", 0.7);
+        .attr("fill", (d, i) => color(i));
+
+    // Add labels to the pie chart
+    svg.selectAll("text")
+        .data(arcs)
+        .enter().append("text")
+        .attr("transform", d => `translate(${arc.centroid(d)})`)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(d => d.data.name);
 }
+
 
 // Function to get filtered data based on selected filters
 function getFilteredData(year, quarter, language) {
@@ -211,7 +308,10 @@ async function loadData() {
 document.getElementById("year").addEventListener("change", updateChart);
 document.getElementById("quarter").addEventListener("change", updateChart);
 document.getElementById("language").addEventListener("change", updateChart);
+document.getElementById("showAllYears").addEventListener("change", updateChart);
 document.getElementById("visualizationType").addEventListener("change", updateChart);
+document.getElementById("byQuarter").addEventListener("change", updateChart); 
+
 
 // Call the function to load data and populate filters when the script starts
 loadData();
